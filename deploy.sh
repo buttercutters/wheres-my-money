c4ae64aa0216890aabed989dcb8939a46ef5c329
Harvey Multani <harvey.multani@gmail.com>
2017-12-01 14:18:07 -0800

    fix deploy typo

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ elif [ "$TRAVIS_BRANCH" == "production" -a $"$TRAVIS_PULL_REQUEST" == "false" ];
 then
   echo "$TRAVIS_BRANCH"
   npm run build
-  ./node_modules/.bin/firebase deploy -P dev --token $FIREBASE_PRODUCTION_DEPLOY_TOKEN 
+  ./node_modules/.bin/firebase deploy -P production --token $FIREBASE_PRODUCTION_DEPLOY_TOKEN 
 else
   echo "Will only deploy if not a pull request and branch is either master or staging"
   exit 0
